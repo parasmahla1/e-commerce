745c95cc2c9da89460f24811c91b91ad5f48e5d0
Paras <parasmahla80@gmail.com>
2024-03-29 10:39:55 +0530

    Setup of image url builder

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,8 +1,15 @@
 import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
 
-export const sanityClient = createClient({
+export const client = createClient({
   projectId: "vege5s5z",
   dataset: 'production',
   apiVersion: "2024-03-20",
   useCdn: true,
 });
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
